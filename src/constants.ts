94fd53d432d4747eeca25ad876ef06609b1f669c
Hunter Clarke <hunter@getmatter.app>
2022-10-27 17:32:09 -0600

    fixes typo in constants

--- src/constants.ts
+++ src/constants.ts
@@ -5,7 +5,7 @@ export const syncCooldownMinutes = 1
 export const syncStaleMinutes = 6
 export const syncFrequencies: { [key: string]: number } = {
   Manual: -1,
-  'Every half hour': 1,
+  'Every half hour': 30,
   'Every hour': 60,
   'Every 12 hours': 60 * 12,
   'Every 24 hours': 60 * 24,
